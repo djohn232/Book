76419950160aba2e6311c373e9f2ef4db0a906b4
djohn232 <82186899+djohn232@users.noreply.github.com>
2021-09-08 21:05:13 -0500

    JUnit Testing Assessment
    Book Class for JUnit Testing

---
+++ src/model/BookLogic.java
@@ -0,0 +1,23 @@
+/**
+ * @author Delanie Johnson - dmjohnson33
+ * CIS175 - Fall 2021
+ * Sep 8, 2021
+ */
+package model;
+
+/**
+ * @author delan
+ *
+ */
+public class BookLogic {
+
+	public boolean isFiction(Book book) {
+		
+		return true;
+	}
+	
+	public boolean isFemaleAuthor(Book book) {
+		
+		return true;
+	}
+}
---
+++ src/tests/AllTests.java
@@ -0,0 +1,20 @@
+/**
+ * @author Delanie Johnson - dmjohnson33
+ * CIS175 - Fall 2021
+ * Sep 8, 2021
+ */
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author delan
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ BookTester.class, BookTesterPrint.class })
+public class AllTests {
+
+}
